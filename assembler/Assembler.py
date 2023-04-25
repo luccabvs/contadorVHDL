41da50ea85243eac9fad9a313af45383c6fce5d6
luccabvs <luccabvs@al.insper.edu.br>
2023-04-24 22:23:44 -0300

    assembler done

--- assembler/Assembler.py
+++ assembler/Assembler.py
@@ -2,81 +2,76 @@ inputASM = 'ASM.txt' #Arquivo de entrada de contém o assembly
 outputBIN = 'BIN.txt' #Arquivo de saída que contém o binário formatado para VHDL
 outputMIF = 'initROM.mif' #Arquivo de saída que contém o binário formatado para .mif
 
-noveBits = True
-
 #definição dos mnemônicos e seus
 #respectivo OPCODEs (em Hexadecimal)
 mne =	{ 
-       "NOP":   "0",
-       "LDA":   "1",
-       "SOMA":  "2",
-       "SUB":   "3",
-       "LDI":   "4",
-       "STA":   "5",
-       "JMP":   "6",
-       "JEQ":   "7",
-       "CEQ":   "8",
-       "JSR":   "9",
-       "RET":   "A",
+       "NOP":   'NOP & "',
+       "LDA":   'LDA & "',
+       "SOMA":  'SOMA & "',
+       "SUB":   'SUB & "',
+       "LDI":   'LDI & "',
+       "STA":   'STA & "',
+       "JMP":   'JMP & "',
+       "JEQ":   'JEQ & "',
+       "CEQ":   'CEQ & "',
+       "JSR":   'JSR & "',
+       "RET":   'RET & "',
 }
 
 #Converte o valor após o caractere arroba '@'
-#em um valor hexadecimal de 2 dígitos (8 bits)
-def  converteArroba(line):
-    line = line.split('@')
-    line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-    line = ''.join(line)
-    return line
-    
-#Converte o valor após o caractere arroba '@'
-#em um valor hexadecimal de 2 dígitos (8 bits) e...
+#em um valor hexadecimal de 2 dígitos (9 bits) e...
 #concatena com o bit de habilita 
+
+def transformaLabel(line):
+    line = line.split('@')
+    for label in labels.keys():
+        if label in line[1]:
+            print(label)
+            line[1] = labels[label]
+    line = '@'.join(line)
+    print(line)
+    return line
+
 def  converteArroba9bits(line):
     line = line.split('@')
     if(int(line[1]) > 255 ):
         line[1] = str(int(line[1]) - 256)
         line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-        line[1] = "\" & '1' & x\"" + line[1]
+        line[1] = "& '1' & x\"" + line[1]
     else:
         line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-        line[1] = "\" & '0' & x\"" + line[1]
+        line[1] = "& '0' & x\"" + line[1]
     line = ''.join(line)
     return line
  
 #Converte o valor após o caractere cifrão'$'
-#em um valor hexadecimal de 2 dígitos (8 bits) 
-def  converteCifrao(line):
-    line = line.split('$')
-    line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-    line = ''.join(line)
-    return line
-    
+#em um valor hexadecimal de 2 dígitos (9 bits) 
 def  converteCifrao9bits(line):
     line = line.split('$')
     if(int(line[1]) > 255 ):
         line[1] = str(int(line[1]) - 256)
         line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-        line[1] = "\" & '1' & x\"" + line[1]
+        line[1] = "& '1' & x\"" + line[1]
     else:
         line[1] = hex(int(line[1]))[2:].upper().zfill(2)
-        line[1] = "\" & '0' & x\"" + line[1]
+        line[1] = "& '0' & x\"" + line[1]
     line = ''.join(line)
     return line
         
 #Define a string que representa o comentário
 #a partir do caractere cerquilha '#'
 def defineComentario(line):
-    if '#' in line:
-        line = line.split('#')
-        line = line[0] + "\t#" + line[1]
+    if '--' in line:
+        line = line.split('--')
+        line = line[0] + "\t--" + line[1]
         return line
     else:
         return line
 
-#Remove o comentário a partir do caractere cerquilha '#',
+#Remove o comentário a partir do caractere '--',
 #deixando apenas a instrução
 def defineInstrucao(line):
-    line = line.split('#')
+    line = line.split('--')
     line = line[0]
     return line
     
@@ -89,8 +84,14 @@ def trataMnemonico(line):
     line[0] = mne[line[0]]
     line = "".join(line)
     return line
-    
 
+def labelDict(line):
+    labels_dict = {}
+    for id, line in enumerate(lines):
+        if '!' in line:
+            label = line.strip()[1:]
+            labels_dict[label] = str(id)
+    return labels_dict
 
 with open(inputASM, "r") as f: #Abre o arquivo ASM
     print(inputASM)
@@ -103,49 +104,37 @@ with open(outputBIN, "w+") as f:  #Abre o destino BIN
     
     for line in lines:        
         
-        #Verifica se a linha começa com alguns caracteres invalidos ('\n' ou ' ' ou '#')
-        if (line.startswith('\n') or line.startswith(' ') or line.startswith('#')):
-            line = line.replace("\n", "")
-            print("-- Sintaxe invalida" + ' na Linha: ' + ' --> (' + line + ')') #Print apenas para debug
+        #Exemplo de linha => 1. JSR @14 #comentario1
+        comentarioLine = defineComentario(line).replace("\n","") #Define o comentário da linha. Ex: #comentario1
+        instrucaoLine = defineInstrucao(line).replace("\n","") #Define a instrução. Ex: JSR @14
         
-        #Se a linha for válida para conversão, executa
-        else:
-            
-            #Exemplo de linha => 1. JSR @14 #comentario1
-            comentarioLine = defineComentario(line).replace("\n","") #Define o comentário da linha. Ex: #comentario1
-            instrucaoLine = defineInstrucao(line).replace("\n","") #Define a instrução. Ex: JSR @14
-            
-            instrucaoLine = trataMnemonico(instrucaoLine) #Trata o mnemonico. Ex(JSR @14): x"9" @14
-                  
-            if '@' in instrucaoLine: #Se encontrar o caractere arroba '@' 
-                if noveBits == False:
-                    instrucaoLine = converteArroba(instrucaoLine) #converte o número após o caractere Ex(JSR @14): x"9" x"0E"
-                else:
-                    instrucaoLine = converteArroba9bits(instrucaoLine) #converte o número após o caractere Ex(JSR @14): x"9" x"0E"
-                    
-            elif '$' in instrucaoLine: #Se encontrar o caractere cifrao '$'
-                if noveBits == False:
-                    instrucaoLine = converteCifrao(instrucaoLine) #converte o número após o caractere Ex(LDI $5): x"4" x"05"
-                else:
-                    instrucaoLine = converteCifrao9bits(instrucaoLine) #converte o número após o caractere Ex(LDI $5): x"4" x"05"
+        # instrucaoLine = trataMnemonico(instrucaoLine) #Trata o mnemonico. Ex(JSR @14): x"9" @14
+        labels = labelDict(line)
+
+        # if '!' in line:
+        #     labels_dict = gotoLabel(line)
+        #     continue
                 
-            else: #Senão, se a instrução nao possuir nenhum imediato, ou seja, nao conter '@' ou '$'
-                instrucaoLine = instrucaoLine.replace("\n", "") #Remove a quebra de linha
-                if noveBits == False:
-                    instrucaoLine = instrucaoLine + '00' #Acrescenta o valor x"00". Ex(RET): x"A" x"00"
-                else:
-                    instrucaoLine = instrucaoLine + "\" & " + "\'0\' & " + "x\"00" #Acrescenta o valor x"00". Ex(RET): x"A" x"00"
-                
+        if '!' in line or line == '\n':
+            instrucaoLine = "NOP"
+
+        if '@' in instrucaoLine: #Se encontrar o caractere arroba '@' 
+            instrucaoLine = transformaLabel(instrucaoLine)
+            print(instrucaoLine)
+            instrucaoLine = converteArroba9bits(instrucaoLine) #converte o número após o caractere Ex(JSR @14): x"9" x"0E"
+        elif '$' in instrucaoLine: #Se encontrar o caractere cifrao '$'
+            instrucaoLine = converteCifrao9bits(instrucaoLine) #converte o número após o caractere Ex(LDI $5): x"4" x"05"
             
-            line = 'tmp(' + str(cont) + ') := x"' + instrucaoLine + '";\t-- ' + comentarioLine + '\n'  #Formata para o arquivo BIN
-                                                                                                       #Entrada => 1. JSR @14 #comentario1
-                                                                                                       #Saída =>   1. tmp(0) := x"90E";	-- JSR @14 	#comentario1
-                                        
-            cont+=1 #Incrementa a variável de contagem, utilizada para incrementar as posições de memória no VHDL
-            f.write(line) #Escreve no arquivo BIN.txt
+        else: #Senão, se a instrução nao possuir nenhum imediato, ou seja, nao conter '@' ou '$'
+            instrucaoLine = instrucaoLine.replace("\n", "") #Remove a quebra de linha
+            instrucaoLine = instrucaoLine + "& " + "\'0\' & " + "x\"00" #Acrescenta o valor x"00". Ex(RET): x"A" x"00"
             
-            print(line,end = '') #Print apenas para debug
-            
+        
+        line = 'tmp(' + str(cont) + ') := ' + instrucaoLine + ';\t-- ' + comentarioLine + '\n'  #Formata para o arquivo BIN
+                                                                                                    #Entrada => 1. JSR @14 #comentario1
+                                                                                                    #Saída =>   1. tmp(0) := x"90E";	-- JSR @14 	#comentario1
+        cont+=1 #Incrementa a variável de contagem, utilizada para incrementar as posições de memória no VHDL
+        f.write(line) #Escreve no arquivo BIN.txt           
 
             
 ############################             
@@ -173,9 +162,12 @@ with open(outputMIF, "w") as f:  #Abre o destino MIF
         cont = cont + 1         #Incrementa varíavel de contagem
         
     for line in lines:
-    
-        replacements = [('t', ''), ('m', ''), ('p', ''), ('(', ''), (')', ''), ('=', ''), ('x', ''), ('"', '')] #Define os caracteres que serão excluídos
         
+            # f.write.replace('!' + label, 'NOP') #Substitui o label por NOP
+            # f.write.replace('@' + label, Labels[label]) #Substitui o label pela Instrucao
+
+        replacements = [('t', ''), ('m', ''), ('p', ''), ('(', ''), (')', ''), ('=', ''), ('x', ''), ('"', '')] #Define os caracteres que serão excluídos
+
         for char, replacement in replacements:
             if char in line:
                 line = line.replace(char, replacement) #Remove os caracteres que foram definidos
