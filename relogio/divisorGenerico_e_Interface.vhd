f12e2bbd1f181a70635566fd56d9eddaed61278f
felipebpl <felipebpl@al.insper.edu.br>
2023-05-08 16:44:29 -0300

    base de tempo

--- relogio/divisorGenerico_e_Interface.vhd
+++ relogio/divisorGenerico_e_Interface.vhd
@@ -3,26 +3,45 @@ USE ieee.std_logic_1164.ALL;
 use ieee.numeric_std.all;
 
 entity divisorGenerico_e_Interface is
+	
    port(clk      :   in std_logic;
       habilitaLeitura : in std_logic;
       limpaLeitura : in std_logic;
+		SelMux : in std_logic; 
       leituraUmSegundo :   out std_logic_vector (7 downto 0)
+		
    );
 end entity;
 
 architecture interface of divisorGenerico_e_Interface is
   signal sinalUmSegundo : std_logic;
-  signal saidaclk_reg1seg : std_logic;
+  signal saidaclk_reg1seg: std_logic; 
+  signal saidaclk_fast: std_logic; 
+  signal tempo  : std_logic;
 begin
 
 baseTempo: entity work.divisorGenerico
            generic map (divisor => 25000000)   -- divide por 10.
            port map (clk => clk, saida_clk => saidaclk_reg1seg);
+			  
+			  
+baseFast: entity work.divisorGenerico
+           generic map (divisor => 25000)   -- divide por 10.
+           port map (clk => clk, saida_clk => saidaclk_fast);	
+	
+fastMUX :  entity work.muxFast  generic map (larguraDados => 1)
+        port map( entradaA_MUX => saidaclk_reg1seg,
+                 entradaB_MUX =>  saidaclk_fast,
+                 seletor_MUX => SelMux,
+                 saida_MUX => tempo);
+				
 
 registraUmSegundo: entity work.flipFlop
-   port map (DIN => '1', DOUT => sinalUmSegundo,
-         ENABLE => '1', CLK => saidaclk_reg1seg,
-         RST => limpaLeitura);
+   port map (DIN => '1', 
+				DOUT => sinalUmSegundo,
+				ENABLE => '1', 
+				CLK => tempo,
+				RST => limpaLeitura);
 
 -- Faz o tristate de saida:
 leituraUmSegundo <= "0000000" & sinalUmSegundo when habilitaLeitura = '1' else "ZZZZZZZZ";
---
+++ relogio/output_files/relogio.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 20.1.0 Build 711 06/05/2020 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(5CEBA4F23) Path("C:/Users/felem/OneDrive - Insper - Institudo de Ensino e Pesquisa/Documentos/2023.1/descomp/contadorVHDL/relogio/output_files/") File("relogio.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
